eddc1460debbdad3c0799ddf19f8ac7ca0fa58b9
Zaiii12 <152398507+Zaiii12@users.noreply.github.com>
2024-10-28 00:14:13 +0800

    Hello

--- Main.php
+++ Main.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Project</title>
+    <!-- Add any CSS files or libraries here -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<html>
+<body>
+
+
 <?php
 
-echo 'test' . '<br>';
 
 ?>
+
+</body>
+</html>
