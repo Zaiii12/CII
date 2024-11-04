cb4ebd56dd6e3a2798ab4275ed5217eb38f40d6f
Zaiii12 <152398507+Zaiii12@users.noreply.github.com>
2024-11-04 20:22:42 +0800

    Structure

--- Main.php
+++ Main.php
@@ -4,7 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CII Project</title>
     
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="MainHTML.css">
+    <!-- Since this is main HTML page we use MainHTML.css
+        If we're going to design the Dono/Status pages please use Dono.css
+        Possibly make a few more .css files for the Members and Thank you page
+        for seperate customization if needed. -->
 </head>
 <html>
 <body>
