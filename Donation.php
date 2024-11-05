683278915ab2497e62b9f4bb2a84cf2858880912
Zaiii12 <152398507+Zaiii12@users.noreply.github.com>
2024-11-05 23:10:08 +0800

    Changed HTML Layout

---
+++ Donation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CII Project</title>
+    <link rel="stylesheet" href="C1.css">
+</head>
+
+<body>
+    <h1 class="h1">Community Involvement Initiative</h1>
+
+    <nav class="nav">
+        <div>
+            <ul>
+                <li><a href="Main.php">Home</a></li>
+                <li><a href="Donation.php">Donate</a></li>
+                <li><a href="Aboutus.html">About Us</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <h2 class = "donate" >Please Donate to our Cause!</h2>
+
+    <div class = "boxes">
+        <div class = "box">Food Pic <!-- Buttons to be added -->
+            <a href ="FoodST.php">Donate Food!</a>
+        </div>
+        <div class = "box">Money Pic <!-- Buttons to be added -->
+            <a href ="MoneyST.php">Donate Money!</a>
+        </div>
+        <div class = "box">Clothes Pic <!-- Buttons to be added -->
+            <a href ="ClothesST.php">Donate Clothes!</a>
+        </div>
+    </div>
+
+    <div class = "blurb">
+        <P>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium impedit debitis itaque ut, eveniet cum possimus nesciunt voluptate inventore delectus earum eligendi minus quod quo distinctio iste illum quidem quas.</P>
+    </div>
+</body>
+</html>
