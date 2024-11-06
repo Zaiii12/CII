cd081329df25172a33ac09b96afef6a8766b56b5
Zaiii12 <152398507+Zaiii12@users.noreply.github.com>
2024-11-06 18:57:40 +0800

    DT/ST

--- ClothesST.php
+++ ClothesST.php
@@ -8,10 +8,32 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="Stylesheet" href="Dono.css">
+<link rel="Stylesheet" href="C2.css">
+<h1 class="h1">Community Involvement Initiative</h1>
 </head>
 <body>
 
-</body>
+    <nav class="nav">
+        <div>
+            <ul>
+                <li><a href="Main.php">Home</a></li>
+                <li><a href="Donation.php">Donate</a></li>
+                <li><a href="Aboutus.html">About Us</a></li>
+            </ul>
+        </div>
+    </nav>
+    <h2>Donate to the Community.</h2>
 
+    <div class ="boxleft">
+        <div class ="box">Put Stats here</div>
+    </div>
+
+    <div class ="desc">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui quasi voluptatem modi, commodi iusto pariatur laborum non officia. Officia quas corrupti est, a non libero repudiandae optio. Quasi, officiis ipsa?</p>
+    </div>
+
+    <div class="btn">
+        <a href="ClothesDT.php">Go to Donation Page</a>
+    </div>
+</body>
 </html>
--- FoodDT.php
+++ FoodDT.php
@@ -8,9 +8,23 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="Stylesheet" href="Dono.css">
-</head>
+<link rel="Stylesheet" href="C2.css">
+<h1 class="h1">Community Involvement Initiative</h1>
 <body>
 
+    <nav class="nav">
+        <div>
+            <ul>
+                <li><a href="Main.php">Home</a></li>
+                <li><a href="Donation.php">Donate</a></li>
+                <li><a href="Aboutus.html">About Us</a></li>
+            </ul>
+        </div>
+    </nav>
+    <h2 class = "dt" >Please fill out these forms</h2>
+
+    <div class ="forms">
+        <form></form>
+    </div>
 </body>
 </html>
