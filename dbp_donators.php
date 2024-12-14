09a8e86d1d8eec9c180011ee32b06c2ad9b209b5
Zaii12 <152398507+Zaiii12@users.noreply.github.com>
2024-12-14 19:47:07 +0800

    Dbp_donators

---
+++ dbp_donators.php
@@ -0,0 +1,40 @@
+<?php
+
+include 'Process/db_connect.php';
+session_start();  
+$db = new Database(); 
+$conn = $db->conn; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $d_firstname = $conn->real_escape_string($_POST['d_firstname']);
+    $d_lastname = $conn->real_escape_string($_POST['d_lastname']);
+    $d_phone = $conn->real_escape_string($_POST['d_phone']);
+    $username = $conn->real_escape_string($_POST['username']);
+    $password = $conn->real_escape_string($_POST['password']);
+
+    $sql_donator = "INSERT INTO donator (d_firstname, d_lastname, d_phone, username, password) 
+                    VALUES ('$d_firstname', '$d_lastname', '$d_phone', '$username', '$password')";
+
+    if ($conn->query($sql_donator) === TRUE) {
+        $donator_id = $conn->insert_id;
+
+        $sql_user = "INSERT INTO users (username, password, donator_id) 
+                     VALUES ('$username', '$password', '$donator_id')";
+
+        if ($conn->query($sql_user) === TRUE) {
+            header("Location: Login.php");
+            exit();
+        } else {
+
+            echo "Error inserting into users table: " . $conn->error;
+        }
+
+    } else {
+
+        echo "Error inserting into donator table: " . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
